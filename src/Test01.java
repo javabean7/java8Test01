dd581f9adc3cc9404a0cee8b68ee287c43239574
javabean7 <java.c@live.com>
2014-08-26 08:56:40 +0800

    01

---
+++ src/Test01.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ASUS
+ * Date: 14-8-22
+ * Time: 上午8:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test01 {
+    @Test
+    public void test() {
+        System.out.println(2222);
+        String a = "";
+    }
+}
